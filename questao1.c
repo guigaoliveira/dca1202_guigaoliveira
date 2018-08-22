b084faa7634a114b1b9c92761ff5a40a19b5f579
guigaoliveira <gmbo182@hotmail.com>
2018-08-22 10:15:42 -0300

    q1

---
+++ questao1.c
@@ -0,0 +1,2 @@
+Ponteiros podem ser úteis para alocação dinâmica de memória, trocar de dados entre funções,
+    permitir a criação de estrutura de dados dinâmica, acessar elementos de um array, etc.Eles permitem armazenar o endereço das variáveis ​​ou um local da memória.
